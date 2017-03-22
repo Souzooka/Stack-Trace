cd900f076cf7dfc5c77d9f772ff4bc30f7881eb0
Dakota <bazookaharwell@gmail.com>
2017-03-22 09:17:04 -1000

    saving onto LL

--- js/index.js
+++ js/index.js
@@ -1,4 +1,13 @@
 window.onload = function() {
+  var llist = linkedListGenerator();
+
+  document.querySelector("#save").addEventListener("click", function() {
+    if (document.querySelector("#input").value !== "") {
+      llist.add(document.querySelector("#input").value);
+    }
+  });
+
+  document.querySelector("#dump")
 
 
 
